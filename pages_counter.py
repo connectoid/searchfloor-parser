dc388333cbc870e7b508e31a2cf9082e42119f4e
Alexander Beley <beleysasha@gmail.com>
2024-06-04 14:26:07 +1200

    Pages counter added

--- pages_counter.py
+++ pages_counter.py
@@ -5,10 +5,10 @@ pathlist = Path('books').rglob('*.pdf')
 overall_pages = 0
 books_count = 0
 for path in pathlist:
-    # because path is object not string
+    books_count += 1
     path_in_str = str(path)
+    print(f'{books_count}. {path_in_str}')
     pdfReader = PyPDF2.PdfReader(path_in_str)
     totalPages = len(pdfReader.pages)           
     overall_pages += totalPages
-    books_count += 1
 print(f'Total book: {books_count}\n Total pages: {overall_pages}\n Average pages in book: {overall_pages / books_count}\n')
